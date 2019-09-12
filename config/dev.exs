6ae64ac56223debb5fd0609d10d2b95514b0382a
Attila Gulyas <toraritte@gmail.com>
2019-09-12 15:55:26 -0700

    Make phx.server port configurable

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :assessments, Assessments.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :assessments, AssessmentsWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: System.get_env("PHX_PORT") || 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
