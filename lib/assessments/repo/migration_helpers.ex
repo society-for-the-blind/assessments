b2e8e41c7e6212cd1a17915319e0051f275af6d0
Attila Gulyas <toraritte@gmail.com>
2019-09-22 08:14:21 -0700

    Add TODO to MigrationHelper

--- lib/assessments/repo/migration_helpers.ex
+++ lib/assessments/repo/migration_helpers.ex
@@ -1,5 +1,10 @@
 defmodule Assessments.Repo.MigrationHelpers do
 
+  # TODO
+  # On a  second thought, this is  probably an overkill.
+  # Just make the enum  types in migration, with copying
+  # the SQL command. More explicit, less assumptions.
+
   use Ecto.Migration
 
   defp make_enum_string(list) do
@@ -8,6 +13,17 @@ defmodule Assessments.Repo.MigrationHelpers do
     |> Enum.join(",")
   end
 
+  @doc """
+  TODO
+  This    function   assumes    that   there    is   a
+  function  in  the  schema  file  with  the  name  of
+  `(type_name)_values` that lists  the members of the
+  enum.
+
+  This module should either be a behaviour to at least
+  make this  requirement more  explicit, or  just kill
+  this module.
+  """
   def make_enum_type(schema, type_name) do
 
     type_name_string = to_string(type_name)
